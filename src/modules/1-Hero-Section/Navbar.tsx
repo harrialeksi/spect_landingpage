5e903bf6494a712dd3f641dbcb5913ee5de25a4b
shinyhawk <hawkpeter0324@gmail.com>
2022-08-16 21:06:32 +0530

    hero & screens

--- src/modules/1-Hero-Section/Navbar.tsx
+++ src/modules/1-Hero-Section/Navbar.tsx
@@ -1,9 +1,9 @@
 export default function Navbar() {
   return (
-    <div className="w-full flex h-18 items-center justify-between top-0 p-6 z-10">
-      <div className="flex flex-row gap-4 text-white text-xl lg:ml-36">
-        <img src="/logo.svg" className="h-8" />
-        <h1>Spect Network</h1>
+    <div className="w-full flex h-18 items-center lg:justify-between sm:justify-center top-0 p-6 z-10">
+      <div className="flex flex-row gap-4 text-white items-center text-xl lg:ml-36">
+        <img src="/logo.svg" className="h-10" />
+        <p className="font-semibold text-xl">Spect Network</p>
       </div>
       <a href="https://circles.spect.network/">
         <button
@@ -18,7 +18,9 @@ export default function Navbar() {
         bg-purple
         bg-opacity-5
         hover:bg-opacity-25
-        duration-700"
+        duration-700
+        sm:hidden
+        md:hidden"
         >
           Launch App
         </button>
--- src/modules/1-Hero-Section/index.tsx
+++ src/modules/1-Hero-Section/index.tsx
@@ -30,14 +30,58 @@ function HeroSection() {
       <VioletBlur className="absolute top-0 left-0 blur-xl" />
       <PinkBlur className="absolute right-0 bottom-48 h-1/6 w-1/6 opacity-50" />
       <PinkBlur className="absolute bottom-36 left-72 h-24 w-24" />
-      <div className="mt-36 mx-auto text-6xl font-bold text-center flex flex-col items-center">
-        <div className="flex flex-col py-4 mb-10 leading-normal">
-          <p>
+      <div className="mt-24 flex flex-col text-center items-center">
+        <div className="flex flex-col mx-auto py-4 mb-10 ">
+          <p className="leading-normal text-6xl font-bold ">
             Buidl the world of <GradientWord>Web3</GradientWord>
+            <br />
+            With Spect
           </p>
-          <p>With Spect</p>
+          <p className="text-center lg:w-2/5 sm:w-4/5 md:4/5 mx-auto mt-4 text-zinc-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
+            placeat voluptate vel blanditiis recusandae illo laboriosam
+            perspiciatis! Tempora veritatis itaque, optio dicta et consequuntur
+            illo corporis perferendis natus, magni id.
+          </p>
         </div>
-        <img src="/earth.svg" className="lg:w-[50%]" />
+
+        <div className="mb-12 flex flex-row gap-6">
+          <a href="https://www.youtube.com/playlist?list=PLdwhfxtIkROmkPUhWwFzwp61ollQf_R44">
+            <button
+              className="
+              px-8
+              py-3
+              rounded-xl
+              text-md
+              text-zinc-400
+              text-bold
+              bg-white
+              bg-opacity-5
+              hover:bg-opacity-25
+              duration-700"
+            >
+              Watch Demo
+            </button>
+          </a>
+          <a href="https://circles.spect.network/">
+            <button
+              className="
+              px-8
+              py-3
+              rounded-xl
+              text-md
+              text-purple
+              text-bold
+              bg-purple
+              bg-opacity-5
+              hover:bg-opacity-25
+              duration-700"
+            >
+              Launch App
+            </button>
+          </a>
+        </div>
+        <img src="/earth.svg" className="lg:w-[50%] mx-auto" />
         <div className="absolute bottom-0 bg-gradient-to-t from-black to h-24 w-full z-60" />
       </div>
     </div>
