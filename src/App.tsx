b06e9edd59fa5ffcd6e63c91ddef599651c2d7ca
shinyhawk <hawkpeter0324@gmail.com>
2022-08-21 19:56:39 +0530

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,6 @@ import About from "./modules/2-About-Spect";
 import SpectVerse from "./modules/3-Spect-Verse";
 import Features from "./modules/4-Features";
 import Footer from "./modules/Footer";
-// import ReactGA from "react-ga";
-
-// const TRACKING_ID = "G-ZE9KPG5TCK";
-// ReactGA.initialize(TRACKING_ID);
 
 function App() {
   return (
@@ -21,28 +17,3 @@ function App() {
 }
 
 export default App;
-
-// export const GA_ANALYTICS_MEASUREMENT_ID = "G-2XJNPKFWB1";
-
-// // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
-// export const pageview = (url: URL): void => {
-//   window.gtag("config", GA_ANALYTICS_MEASUREMENT_ID, {
-//     page_path: url,
-//   });
-// };
-
-// type GTagEvent = {
-//   action: string;
-//   category: string;
-//   label: string;
-//   value: number;
-// };
-
-// // https://developers.google.com/analytics/devguides/collection/gtagjs/events
-// export const event = ({ action, category, label, value }: GTagEvent): void => {
-//   window.gtag("event", action, {
-//     event_category: category,
-//     event_label: label,
-//     value,
-//   });
-// };
