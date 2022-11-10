156d910e3bfd31a19489e00f38923e6090c3dcd9
shinyhawk <hawkpeter0324@gmail.com>
2022-11-11 02:23:38 +0530

    fix: build fix

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(root, 'index.html'),
-        features: resolve(root, 'features', 'index.html'),
       }
     }
   }
